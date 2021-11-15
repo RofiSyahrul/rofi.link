619e1b56637d101a11a376d31191b45054b58e21
RofiSyahrul <srofi44@gmail.com>
2021-11-16 06:37:33 +0700

    refactor: update code style and editor configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,41 @@
+const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   ...(process.env.NODE_ENV === 'production' && { mode: 'jit' }),
+  darkMode: 'class',
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+        sky: colors.sky
+      }
+    }
   },
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    plugin(({ addComponents }) => {
+      /** @type {Record<string, React.CSSProperties>} */
+      const placeholders = {
+        '.placeholder': {
+          color: colors.gray[600]
+        },
+        '.placeholder-dark': {
+          color: colors.gray[300]
+        },
+        '.placeholder-labeled-not-focus': {
+          visibility: 'hidden'
+        },
+        '.placeholder-focus': {
+          visibility: 'visible'
+        }
+      };
+
+      addComponents(placeholders);
+    })
+  ]
 };
