c57e465228a844712f05d865f62201c3190c8cbe
RofiSyahrul <srofi44@gmail.com>
2021-11-20 00:01:25 +0700

    feat: integrate switch mode button with homepage

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import Meta from '@/components/shared/Meta';
+import SwitchModeButton from '@/components/shared/SwitchModeButton';
 
 import ShortenerForm from './ShortenerForm';
 
 export default function HomePage() {
   return (
-    <main className='w-screen h-screen flex items-center justify-center flex-col'>
+    <main className='w-screen h-screen flex items-center justify-center flex-col px-3'>
       <Meta />
+      <div className='fixed top-3 right-3'>
+        <SwitchModeButton />
+      </div>
       <ShortenerForm />
     </main>
   );
