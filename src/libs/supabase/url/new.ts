3001c90010d97763705becb5d8f43fd9c2344fe7
RofiSyahrul <srofi44@gmail.com>
2022-01-09 19:14:32 +0700

    feat: restrict shorten new url using existing path

--- src/libs/supabase/url/new.ts
+++ src/libs/supabase/url/new.ts
@@ -5,7 +5,17 @@ import { supabase } from '../_base';
 import { logError } from '../utils/log-error';
 import { URL_TABLE } from './config';
 
+const reservedSlugs = ['api', 'p'];
+
+function buildAlreadyExistsError(slug: string) {
+  return new ApiError(`${slug} already exists`);
+}
+
 export async function shortenNewUrl(param: ShortenNewUrlParam) {
+  if (reservedSlugs.includes(param.slug)) {
+    throw buildAlreadyExistsError(param.slug);
+  }
+
   const { error } = await supabase.from<UrlModel>(URL_TABLE).insert({
     actual_url: param.actualUrl,
     slug: param.slug,
@@ -14,7 +24,7 @@ export async function shortenNewUrl(param: ShortenNewUrlParam) {
 
   if (error) {
     if (error.message === 'duplicate key value violates unique constraint "urls_slug_key"') {
-      throw new ApiError(`${param.slug} already exists`);
+      throw buildAlreadyExistsError(param.slug);
     }
     logError('shortenNewUrl', error);
     throw error as unknown;
