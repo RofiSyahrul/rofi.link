e3c78c8464052f0bc6902e46255e949755c81cea
RofiSyahrul <srofi44@gmail.com>
2022-01-09 19:11:23 +0700

    refactor: move callback route to /p/callback

--- src/libs/supabase/auth/login-with-google.ts
+++ src/libs/supabase/auth/login-with-google.ts
@@ -5,7 +5,7 @@ import { supabase } from '../_base';
 export async function loginWithGoogle() {
   const response = await supabase.auth.signIn(
     { provider: 'google' },
-    { redirectTo: `${config.appURL}/callback` }
+    { redirectTo: `${config.appURL}/p/callback` }
   );
   return response;
 }
